d2f4d65f7701a1f9ddc1799f8a261756fc93b7c4
ahasan <ahasan@isimarkets.com>
2020-11-09 13:25:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {
+  Line,
+  SteppedLine,
+  PolyLine,
+  Circle,
+  Rectangle
+} from 'draw-shape-reactjs';
+
 
 function App() {
+  const initialCounter = 0;
+  const [counter, setCounter] = useState(initialCounter);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {drawNeurons(counter)}
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prevCounter => ++prevCounter)}>Increment</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
     </div>
   );
+}
+
+let drawNeurons = (neurons_n) => {
+  let neurons = [];
+  let neuron_pos = 200;
+  
+  for (let i = 0; i < neurons_n; i++){
+    neurons.push(<Circle
+                  center={[100, neuron_pos]} radius={15} color='#00701a'
+                />);
+    neuron_pos += 50;
+  }
+
+  return neurons;
 }
 
 export default App;
