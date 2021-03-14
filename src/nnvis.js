f57d666b497cd3e0ed0f1fa0faada70b2399b5a8
auhide <bo0opr@gmail.com>
2021-03-14 20:53:35 +0200

    A better navigation bar; The logo is still missing

--- src/nnvis.js
+++ src/nnvis.js
@@ -8,35 +8,17 @@ import {
     DatasetsComponents 
 } from './widgets/Config';
 
-const style = {
-    navbar: {
-        backgroundColor: null,
-        maxWidth: window.innerWidth / 2,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        border: '2px solid #595959',
-        borderStyle: 'none none solid none'
-    },
-    logo: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-    },
-    button: {
-        marginBottom: -30,
-    }
-}
 
 export function NNVis() {
 
     return (
         <Router>
-            <Toolbar style={style.navbar}>
-                <Button style={style.button}><Link to={'/'} >Config</Link></Button>
-                <Button style={style.button}><Link to={'/conv'} >Conv2D</Link></Button>
-                <img style={style.logo} alt="Missing Logo" src={process.env.PUBLIC_URL + "/logo-nn.png"} />
-                <Button style={style.button}><Link to={'/add-dataset'} >Add Data</Link></Button>
-                <Button style={style.button}><Link to={'/datasets'} >Data</Link></Button>
-            </Toolbar>
+            <ul>
+                <li className="mainText"><Link to={'/'} >Config</Link></li>
+                <li className="mainText"><Link to={'/conv'} >Conv2D</Link></li>
+                <li className="mainText"><Link to={'/preprocess'} >Preprocess</Link></li>
+                <li className="mainText"><Link to={'/datasets'} >Data</Link></li>
+            </ul>
 
             <Switch> 
 
