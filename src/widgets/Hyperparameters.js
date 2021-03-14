7ec448c3c1467ab7e03ef9954a19b515bece84b4
auhide <bo0opr@gmail.com>
2021-03-14 17:32:27 +0200

    The number of epochs' upper bound is set to 500

--- src/widgets/Hyperparameters.js
+++ src/widgets/Hyperparameters.js
@@ -202,24 +202,24 @@ const learningRates = [
 
 const epochMarks = [
     {
+        value: 100,
+        label: '100',
+    },
+    {
         value: 200,
         label: '200',
+    },
+    {
+        value: 300,
+        label: '300',
     },
     {
         value: 400,
         label: '400',
     },
     {
-        value: 600,
-        label: '600',
-    },
-    {
-        value: 800,
-        label: '800',
-    },
-    {
-        value: 1000,
-        label: '1K',
+        value: 500,
+        label: '500',
     },
 ];
 
@@ -551,7 +551,7 @@ function Epochs({ params, dispatch }) {
                     aria-labelledby="discrete-slider-small-steps"
                     step={1}
                     min={1}
-                    max={1000}
+                    max={500}
                     marks={epochMarks}
                     valueLabelDisplay="auto"
                 />
