c53e43068095c67a073fd5cdbe151b86a5d50599
auhide <bo0opr@gmail.com>
2021-05-17 21:01:29 +0300

    The base address of the API is moved to a separate variable

--- src/apiEndpoints.js
+++ src/apiEndpoints.js
@@ -1,6 +1,8 @@
 
 // The NN API Endpoints that are of use now:
-export const datasetsNamesEndpoint = "http://localhost:5000/datasets/names";
-export const datasetsInformationEndpoint = "http://localhost:5000/datasets/info/";
-export const datasetsEndpoint = "http://localhost:5000/datasets/";
-export const architectureEndpoint = "http://localhost:5000/architecture";
+const baseUrl = "http://localhost:5000/"
+
+export const datasetsNamesEndpoint = baseUrl + "datasets/names";
+export const datasetsInformationEndpoint = baseUrl + "datasets/info/";
+export const datasetsEndpoint = baseUrl + "datasets/";
+export const architectureEndpoint = baseUrl + "architecture";
