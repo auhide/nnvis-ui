19e190e49e5bfc7e0177edba89abe7430259bfa0
auhide <bo0opr@gmail.com>
2021-09-16 19:02:11 +0300

    Taking the API host as an env. variable

--- src/apiEndpoints.js
+++ src/apiEndpoints.js
@@ -1,6 +1,10 @@
 
 // The NN API Endpoints that are of use now:
-const baseUrl = "http://localhost:5000/"
+const baseUrl = process.env.REACT_APP_BASE_API_URL;
+
+if (typeof baseUrl !== 'undefined') {
+    baseUrl = "http://localhost:5000/";
+}
 
 export const datasetsNamesEndpoint = baseUrl + "datasets/names";
 export const datasetsInformationEndpoint = baseUrl + "datasets/info/";
